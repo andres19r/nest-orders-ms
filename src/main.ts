6cea5580277050b52fa55dba745639a94942ed7c
Andres Rivero <andres19rivero@gmail.com>
2025-12-05 16:38:03 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Orders-Main');
+  const app = await NestFactory.create(AppModule);
+  await app.listen(envs.port);
+  logger.log(`Orders Microservice running on port ${envs.port}`);
+}
+bootstrap();
