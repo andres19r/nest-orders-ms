04301c54bf84d28c16c6f69c8b6e9b313c28d6e2
Andres Rivero <andres19rivero@gmail.com>
2025-12-08 17:34:14 -0400

    feat(db)!: set up postgres db with prisma and create order model

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { envs } from 'src/config';
 
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('OrdersService');
+
+  constructor() {
+    const adapter = new PrismaPg({
+      url: envs.databaseUrl,
+    });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
