00e9dd0833aa10601a6d9206617448c7110685f8
Andres Rivero <andres19rivero@gmail.com>
2025-12-11 16:17:13 -0400

    feat(orders): refactor order service to use pg Pool and update create method

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
+import { CreateOrderDto } from './dto';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 import { envs } from 'src/config';
 
 @Injectable()
@@ -9,9 +10,10 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('OrdersService');
 
   constructor() {
-    const adapter = new PrismaPg({
-      url: envs.databaseUrl,
+    const pool = new Pool({
+      connectionString: envs.databaseUrl,
     });
+    const adapter = new PrismaPg(pool);
     super({ adapter });
   }
 
@@ -21,7 +23,9 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   }
 
   create(createOrderDto: CreateOrderDto) {
-    return createOrderDto;
+    return this.order.create({
+      data: createOrderDto,
+    });
   }
 
   findAll() {
