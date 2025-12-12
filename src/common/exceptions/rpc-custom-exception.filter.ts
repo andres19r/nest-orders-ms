3136607f9e7a4ea60c8698f84767b493f84dada9
Andres Rivero <andres19rivero@gmail.com>
2025-12-12 16:29:17 -0400

    feat(orders): implement pagination and status filtering for order retrieval

---
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -0,0 +1,28 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { Response } from 'express';
+
+@Catch(RpcException)
+export class RpcCustomExceptionFilter implements ExceptionFilter {
+  catch(exception: RpcException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response: Response = ctx.getResponse();
+
+    const rpcError = exception.getError();
+
+    if (
+      typeof rpcError === 'object' &&
+      'status' in rpcError &&
+      'message' in rpcError
+    ) {
+      const rpcStatus = rpcError.status as number;
+      const status = isNaN(+rpcStatus) ? 400 : +rpcStatus;
+      return response.status(status).json(rpcError);
+    }
+
+    response.status(400).json({
+      status: 400,
+      message: rpcError,
+    });
+  }
+}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import { envs } from 'src/config';
+import { RpcException } from '@nestjs/microservices';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -28,11 +30,43 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+    const totalPages = await this.order.count({
+      where: {
+        status: orderPaginationDto.status,
+      },
+    });
+    const currentPage = orderPaginationDto.page!;
+    const perPage = orderPaginationDto.limit!;
+
+    return {
+      meta: {
+        total: totalPages,
+        page: currentPage,
+        lastPage: Math.ceil(totalPages / perPage),
+      },
+      data: await this.order.findMany({
+        skip: (currentPage - 1) * perPage,
+        take: perPage,
+        where: {
+          status: orderPaginationDto.status,
+        },
+      }),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string) {
+    const order = await this.order.findUnique({
+      where: { id },
+    });
+
+    if (!order) {
+      throw new RpcException({
+        message: `Order with id ${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
+    }
+
+    return order;
   }
 }
